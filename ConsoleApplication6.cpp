d4c476a4b9763b0fd62d1bb28de64b763fe34388
jiangyouxiaosheng <73007788+jiangyouxiaosheng@users.noreply.github.com>
2021-10-13 13:47:10 +0900

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,52 @@
+﻿#include <stdio.h>
+
+
+class TS
+{
+public:
+	       TS(int x = 1000)
+	       {
+		         nArrya = new int[x];
+				 
+	       }
+		   
+		   void Set(int setnArrya);
+		   void Get(int getnArrya);
+
+
+
+	        ~TS()
+	        {
+		          delete[] nArrya;    		        
+	        }
+
+private:
+	        int* nArrya;
+
+
+};
+int main()
+{
+	        TS ts;
+
+
+
+	        for (int i = 0; i < 1000; i++)
+	        {
+			        ts.Set(i);
+	        }
+			for (int i = 0; i < 1000; i++)
+			{
+				    ts.Get(i);
+			}
+}
+
+void TS::Set(int setnArrya)
+{
+	nArrya[setnArrya] = setnArrya;
+}
+
+void TS::Get(int getnArrya)
+{
+	printf("%d\n", nArrya[getnArrya]);
+}
