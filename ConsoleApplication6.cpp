624fafab949dfbfd3fa3157b1d3b9787d8e01f66
jiangyouxiaosheng <73007788+jiangyouxiaosheng@users.noreply.github.com>
2021-10-13 12:59:42 +0900

    Add files via upload

---
+++ ConsoleApplication6.cpp
@@ -0,0 +1,34 @@
+﻿#include <stdio.h>
+
+
+class TS
+{
+public:
+	       TS()
+	        {
+		         newArray = new int[1000];
+	        }
+	        ~TS()
+	         {
+		          delete[] newArray;
+		          newArray = nullptr;
+		          printf("%p\n", newArray);
+	        }
+
+public:
+	        int* newArray;
+
+
+};
+int main()
+{
+	        TS ts;
+
+
+
+	        for (int i = 0; i < 1000; i++)
+	        {
+		          ts.newArray[i] = i * 1;
+		          printf("%d\n",ts.newArray[i]);
+	        }
+}
